4e27e2ddae9e3dc12e71b54403f994beb13b3928
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2025-09-08 20:11:28 -0300

    menu e timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI TimerText;
+    [SerializeField] float remainingTime;
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+
+            remainingTime -= Time.deltaTime;
+
+        }
+        else if (remainingTime < 0)
+        {
+            remainingTime = 0;
+
+        }
+       
+        int minutos = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        TimerText.text = string.Format("{0:00}:{1:00}", minutos, seconds);
+    }
+}
